ff1155f1f42f99374856cc87b43c9a9df233f23f
nddszy <1836107093@qq.com>
2022-12-10 15:04:28 +0800

    update html

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const trigger = document.getElementById("trigger"); //根据id获取输入元素
 const abyss = document.getElementById("abyss"); //根据id获取输入元素
 const context = document.getElementById("context"); //根据id获取输入元素
 const url = "https://api.github.com/repos/nddszy/nddszy.github.io/issues?filter=updated";
+var codeStr = null; //从github获得的密文
 
 if (trigger){
     trigger.addEventListener("click", getKey);
@@ -11,30 +12,37 @@ if (abyss) {
     abyss.addEventListener("click", getCode);
 }
 
-var codeStr = null;
-$.getJSON(url, function (data) {
-    if (data.length > 0) {
-        codeStr = data[0].body;
-    }
-    else {
-        console.log("访问失效");
-    }
-});
-
 var clickCount = 0;
 function getKey() {
     clickCount++;
     if (clickCount == 3) {
         clickCount = 0;
-        var keyStr = prompt("请输入密钥：");
-        if (keyStr && codeStr) {
-            context.innerHTML = fromCode(codeStr, keyStr);
-            alert("尘封的秘密已经浮现");
+        if (!codeStr) {
+            $.getJSON(url, function (data) {
+                if (data.length > 0) {
+                    codeStr = data[0].body;
+                    ShowStr();
+                }
+                else {
+                    console.log("访问失效");
+                }
+            });
         }
         else {
-            alert("错误！请核对密钥或联系管理员");
+            ShowStr();
         }
     } 
+}
+
+function ShowStr() {
+    var keyStr = prompt("请输入密钥：");
+    if (keyStr && codeStr) {
+        context.innerHTML = fromCode(codeStr, keyStr);
+        alert("尘封的秘密已经浮现");
+    }
+    else {
+        alert("错误！请核对密钥或联系管理员");
+    }
 }
 
 var callCount = 0;
@@ -43,7 +51,7 @@ function getCode() {
     if (callCount == 3) {
         callCount = 0;
         var keyStr = prompt("请输入密钥：");
-        var contentStr = prompt("请输入知识：");
+        var contentStr = prompt("请输入谜底：");
         if (keyStr && contentStr) {
             context.innerHTML = toCode(contentStr, keyStr);
             alert("阴暗的种子已经埋下");
